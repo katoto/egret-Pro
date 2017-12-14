b8a1131b6746a5430a8057c1f4a340910e4a1591
xiezg <xiezg@500wan.com>
2017-12-14 14:10:55 +0800

    '1214'

--- src/utils/util.js
+++ src/utils/util.js
@@ -1,4 +1,51 @@
 
+// class AssetAdapter implements eui.IAssetAdapter{
+//     /**
+//      *  解析素材
+//      * @param source 待解析的新素材
+//      * @param compFunc 解析完成回调函数
+//      * @param thisObject callback 的this 引用
+//      * 
+//      */
+//     public getAsset( source:string, compFunc:Function,thisObject:any ):void{
+//         function onGetRes( data:any ):void{
+//             compFunc.call( thisObject ,data,source )
+//         }
+//         if(RES.hasRes( source )){
+//             let data = RES.getRes( source );
+//             if( data ){
+//                 onGetRes( data );
+//             }else{
+//                 RES.getResAsync( source ,onGetRes ,this );
+//             }
+//         }else{
+//             RES.getResByUrl( source,onGetRes,this,RES.ResourceItem.TYPE_IMAGE );
+//         }
+//     }
+// }
+
+// class ThemeAdapter implements eui.IThemeAdapter{
+//     /**
+//      *  解析主题
+//      *  @param url 待解析的主题url
+//      *  @param compFunc 解析完成回调函数， eg compFunc( e:egret.Event ):void;
+//      *  @param errorFunc 解析失败回调函数 ， eg errorFunc()：void;
+//      *  @param thisObject 回调的this 引用
+//      */
+//     public getTheme( url:string, compFunc:Function,errorFunc:Function ,thisObject:any):void{
+//         function onGetRes(e:string):void{
+//             compFunc.call(thisObject,e);
+//         }
+//         function onError(e:RES.ResourceEvent):void{
+//             if(e.resItem.url == url){
+//                 RES.removeEventListener(RES.ResourceEvent.ITEM_LOAD_ERROR,onError,null);
+//                 errorFunc.call(thisObject);
+//             }
+//         }
+//         RES.addEventListener( RES.ResourceEvent.ITEM_LOAD_ERROR,onError,null );
+//         RES.getResByUrl( url ,onGetRes ,this,RES.ResourceItem.TYPE_TEXT );
+//     }
+// }
 
 /**
  *  数组转对象
